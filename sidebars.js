ed59e4daedd258fee7a023c52aaeb54c562576c9
Michael Stromberg <mstromberg@illumina.com>
2020-11-13 11:39:08 -0800

    Added Splice AI

--- sidebars.js
+++ sidebars.js
@@ -16,6 +16,7 @@ module.exports = {
         'data-sources/clinvar',
         'data-sources/dbsnp',
         'data-sources/mito-heteroplasmy',
+        'data-sources/splice-ai',
         'data-sources/primate-ai'
       ],
     },
